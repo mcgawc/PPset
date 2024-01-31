316bea6fbee5d1d444069057993cef3c378a0335
swappimittal <148274152+swappimittal@users.noreply.github.com>
2024-01-31 10:03:26 -0800

    Update streamlit_probe_app.py

--- streamlit_probe_app.py
+++ streamlit_probe_app.py
@@ -518,7 +518,7 @@ def main():
 
     probe_dict_seq2 = execute_subprocess_1(seq_2, valid_permutations, tm_range, GC_range, pos_range, len_range, LNA_range, aprox_tm_range, input_seq[seq_1], token)
     st.header("Probes for " + input_seq[seq_2] + " allele")
-    display_probe_data(probe_dict_seq1)
+    display_probe_data(probe_dict_seq2)
 
     if st.button("Export probes 2 to Excel"):
         excel_name = 'probes2'
